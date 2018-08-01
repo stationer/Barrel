LoneFry/GraphiteBarrel

Isolate Graphite/Barrel from legacy repo

src/controllers/DefaultController.php
@@ -1,0 +1,92 @@
+<?php
+/**
+ * Default Controller - leans on Controller's defaults
+ * File : /^/controllers/DefaultController.php
+ *
+ * PHP version 5.6
+ *
+ * @category Graphite
+ * @package  Core
+ * @author   LoneFry <dev@lonefry.com>
+ * @license  CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * @link     http://g.lonefry.com
+ */
+
+namespace Graphite\barrel\controllers;
+
+use Graphite\core\View;
+use Graphite\core\Controller;
+
+/**
+ * DefaultController class - leans on Controller's defaults
+ *
+ * @category Graphite
+ * @package  Core
+ * @author   LoneFry <dev@lonefry.com>
+ * @license  CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * @link     http://g.lonefry.com
+ * @see      /^/lib/Controller.php
+ */
+class DefaultController extends Controller {
+    /** @var string Default action */
+    protected $action = '200';
+
+    /**
+     * Default action for handling 404 errors
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_404(array $argv = array(), array $request = array()) {
+        header("HTTP/1.0 404 File Not Found");
+        $this->action = '404';
+        $this->View->_template = '404.php';
+        $this->View->_header   = 'header.php';
+        $this->View->_footer   = 'footer.php';
+        $this->View->_title    = 'Requested Page Not Found';
+        $this->View->setTemplate('subheader', '');
+
+        return $this->View;
+    }
+
+    /**
+     * Default action for handling 500 errors
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_500(array $argv = array(), array $request = array()) {
+        header("HTTP/1.0 500 Internal Server Error");
+        $this->action = '500';
+        $this->View->_template = '500.php';
+        $this->View->_header   = 'header.php';
+        $this->View->_footer   = 'footer.php';
+        $this->View->_title    = 'Internal Server Error';
+        $this->View->setTemplate('subheader', '');
+
+        return $this->View;
+    }
+
+    /**
+     * Default action for handling 200 OK
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_200(array $argv = array(), array $request = array()) {
+        $this->action = '200';
+        $this->View->_template = '200.php';
+        $this->View->_header   = 'header.php';
+        $this->View->_footer   = 'footer.php';
+        $this->View->_title    = 'Default OK Page';
+        $this->View->setTemplate('subheader', '');
+
+        return $this->View;
+    }
+}

src/data/Config.php
@@ -1,0 +1,125 @@
+<?php
+/**
+ * Log Config Changes
+ * File : /^BCP/models/Config.php
+ *
+ * PHP version 5.3
+ *
+ * @category BetterCarPeople
+ * @package  BCP
+ * @author   Tyler Uebele <tyler.uebele@bettercarpeople.com>
+ * @license  Copyright Better Car People, LLC
+ * @link     http://bettercarpeople.com
+ */
+
+namespace Graphite\Barrel\Data;
+
+use Graphite\core\G;
+use Graphite\core\data\Record;
+use Graphite\core\data\Login;
+use Graphite\core\data\DataBroker;
+
+/**
+ * Config class - for logging config changes
+ *
+ * @category BetterCarPeople
+ * @package  BCP
+ * @author   Tyler Uebele <tyler.uebele@bettercarpeople.com>
+ * @license  Copyright Better Car People, LLC
+ * @link     http://bettercarpeople.com
+ * @see      Record.php
+ */
+class Config extends Record {
+    protected static $table = G_DB_TABL.'ConfigLog';
+    protected static $pkey  = 'log_id';
+    protected static $query = '';
+    protected static $vars  = array(
+        'log_id'        => array('type' => 'i',  'min' => 1, 'guard' => true),
+        'tableName'     => array('type' => 's',  'max' => 255),
+        'login_id'      => array('type' => 'i',  'min' => 1, 'max' => 65535, 'def' => 0),
+        'eventDate'     => array('type' => 'dt', 'def' => NOW),
+        'data'          => array('type' => 'o',  'max' => 65535),
+        'affected_id'   => array('type' => 'i',  'min' => 0),
+        'recordChanged' => array('type' => 'dt', 'min' => 0, 'guard' => true),
+    );
+
+    protected $loginname    = '';
+
+    /**
+     * Wrap the parent constructor and set roles if passed
+     *
+     * @param bool|int|array $a pkey value|set defaults|set values
+     * @param bool           $b Set defaults
+     *
+     * @throws \Exception
+     */
+    public function __construct($a = null, $b = null) {
+        if ('' == static::$query) {
+            self::$query = 'SELECT t.`log_id`, t.`tableName`, t.`login_id`, t.`eventDate`,'
+                .' t.`data`, t.`affected_id`, l.`loginname`'
+                .' FROM `'.self::$table.'` t LEFT JOIN `'.Login::getTable().'` l ON t.`login_id` = l.`login_id`';
+        }
+        if (G::$S->Login) {
+            self::$vars['login_id']['def'] = G::$S->Login->login_id;
+        }
+        if (is_array($a) && isset($a['loginname'])) {
+            $this->loginname = $a['loginname'];
+        }
+        parent::__construct($a, $b);
+    }
+
+    /**
+     * Logs data into the config database
+     *
+     * @param string $table the table name
+     * @param int    $key   the affected_id
+     * @param string $data  the data being saved
+     *
+     * @return string
+     */
+    public static function log($table, $key, $data) {
+        $C = new static(array('tableName' => $table,
+                              'affected_id' => $key,
+                              'data' => $data,
+                              ),
+                        true);
+        return G::build(DataBroker::class)->save($C);
+    }
+
+    /**
+     * Returns the last 100 log entries
+     *
+     * @param string $table the table name
+     * @param int    $key   the affected ID
+     *
+     * @return array
+     */
+    public static function getLog($table, $key) {
+        $C = new static(array('tableName' => $table, 'affected_id' => $key));
+        return $C->search(100, 0, 'log_id', true);
+    }
+
+    /**
+     * Sets the login name after load
+     *
+     * @param array $row the row
+     *
+     * @return array
+     */
+    public function onload(array $row = array()) {
+        if (isset($row['loginname'])) {
+            $this->loginname = $row['loginname'];
+            unset($row['loginname']);
+        }
+        return $row;
+    }
+
+    /**
+     * Returns the login name
+     *
+     * @return string
+     */
+    public function loginname() {
+        return $this->loginname;
+    }
+}

src/templates/header.php
@@ -1,0 +1,57 @@
+<?php use Graphite\Core\G; ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title><?php html($_title); ?></title>
+        <base href="<?php html($_siteURL); ?>">
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<?php foreach ($_meta as $k => $v) { ?>
+        <meta name="<?php html($k)?>" content="<?php html($v)?>">
+<?php }
+      foreach ($_script as $v) { ?>
+        <script type="text/javascript" src="<?php html($v)?>"></script>
+<?php }
+      foreach ($_link as $v) { ?>
+        <link rel="<?php html($v['rel'])?>" type="<?php html($v['type'])?>" href="<?php html($v['href'])?>" title="<?php html($v['title'])?>">
+<?php }
+      echo $_head;
+?>
+    </head>
+    <body>
+        <header id="header">
+            <h1 id="logo"><span><?php html($_siteName)?></span></h1>
+            <div id="login"><?php
+if ($_login_id) {
+        echo 'Hello, '.$_loginname
+            .'. (<a href="'.$_logoutURL.'">Logout</a> | '
+            .'<a href="/Account/edit" title="Your Account Settings">Account</a>)'
+            ;
+} else {
+        echo '(<a id="_loginLink" href="'.$_loginURL.'?_Lbl=Back&amp;_URI='.urlencode($_SERVER["REQUEST_URI"]).'">Login</a>)'
+            .'<script type="text/javascript">document.getElementById(\'_loginLink\').href += encodeURIComponent(location.hash);</script>';
+}
+            ?></div>
+            <nav>
+                <a href="/" title="Home Page">Home</a>
+                <a href="/Home/Contact" title="Contact">Contact</a>
+<?php if (G::$S && G::$S->Login && G::$S->roleTest('Admin')) { ?>
+                <a href="/Admin">Admin</a>
+<?php } ?>
+            </nav>
+            <div class="clear"></div>
+        </header>
+
+<?php G::$V->render('subheader');
+
+if (0 < $v = count($a = G::msg())) { ?>
+        <details id="msg" open="open">
+            <summary><?php echo $v;?> Messages:</summary>
+            <ul>
+<?php foreach ($a as $v) { ?>
+                <li class="<?php echo $v[1]; ?>"><?php echo $v[0]; ?></li>
+<?php } ?>
+            </ul>
+        </details>
+<?php } ?>
+
+        <section id="body">

stationer/Barrel

src/controllers/DefaultController.php
@@ -1,0 +1,92 @@
+<?php
+/**
+ * Default Controller - leans on Controller's defaults
+ * File : /^/controllers/DefaultController.php
+ *
+ * PHP version 5.6
+ *
+ * @category Graphite
+ * @package  Core
+ * @author   LoneFry <dev@lonefry.com>
+ * @license  CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * @link     http://g.lonefry.com
+ */
+
+namespace Stationer\Barrel\controllers;
+
+use Stationer\Graphite\View;
+use Stationer\Graphite\Controller;
+
+/**
+ * DefaultController class - leans on Controller's defaults
+ *
+ * @category Graphite
+ * @package  Core
+ * @author   LoneFry <dev@lonefry.com>
+ * @license  CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * @link     http://g.lonefry.com
+ * @see      /^/lib/Controller.php
+ */
+class DefaultController extends Controller {
+    /** @var string Default action */
+    protected $action = '200';
+
+    /**
+     * Default action for handling 404 errors
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_404(array $argv = array(), array $request = array()) {
+        header("HTTP/1.0 404 File Not Found");
+        $this->action = '404';
+        $this->View->_template = '404.php';
+        $this->View->_header   = 'header.php';
+        $this->View->_footer   = 'footer.php';
+        $this->View->_title    = 'Requested Page Not Found';
+        $this->View->setTemplate('subheader', '');
+
+        return $this->View;
+    }
+
+    /**
+     * Default action for handling 500 errors
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_500(array $argv = array(), array $request = array()) {
+        header("HTTP/1.0 500 Internal Server Error");
+        $this->action = '500';
+        $this->View->_template = '500.php';
+        $this->View->_header   = 'header.php';
+        $this->View->_footer   = 'footer.php';
+        $this->View->_title    = 'Internal Server Error';
+        $this->View->setTemplate('subheader', '');
+
+        return $this->View;
+    }
+
+    /**
+     * Default action for handling 200 OK
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_200(array $argv = array(), array $request = array()) {
+        $this->action = '200';
+        $this->View->_template = '200.php';
+        $this->View->_header   = 'header.php';
+        $this->View->_footer   = 'footer.php';
+        $this->View->_title    = 'Default OK Page';
+        $this->View->setTemplate('subheader', '');
+
+        return $this->View;
+    }
+}

src/data/Config.php
@@ -1,0 +1,125 @@
+<?php
+/**
+ * Log Config Changes
+ * File : /^BCP/models/Config.php
+ *
+ * PHP version 5.3
+ *
+ * @category BetterCarPeople
+ * @package  BCP
+ * @author   Tyler Uebele <tyler.uebele@bettercarpeople.com>
+ * @license  Copyright Better Car People, LLC
+ * @link     http://bettercarpeople.com
+ */
+
+namespace Stationer\Barrel\Data;
+
+use Stationer\Graphite\G;
+use Stationer\Graphite\data\Record;
+use Stationer\Graphite\data\Login;
+use Stationer\Graphite\data\DataBroker;
+
+/**
+ * Config class - for logging config changes
+ *
+ * @category BetterCarPeople
+ * @package  BCP
+ * @author   Tyler Uebele <tyler.uebele@bettercarpeople.com>
+ * @license  Copyright Better Car People, LLC
+ * @link     http://bettercarpeople.com
+ * @see      Record.php
+ */
+class Config extends Record {
+    protected static $table = G_DB_TABL.'ConfigLog';
+    protected static $pkey  = 'log_id';
+    protected static $query = '';
+    protected static $vars  = array(
+        'log_id'        => array('type' => 'i',  'min' => 1, 'guard' => true),
+        'tableName'     => array('type' => 's',  'max' => 255),
+        'login_id'      => array('type' => 'i',  'min' => 1, 'max' => 65535, 'def' => 0),
+        'eventDate'     => array('type' => 'dt', 'def' => NOW),
+        'data'          => array('type' => 'o',  'max' => 65535),
+        'affected_id'   => array('type' => 'i',  'min' => 0),
+        'recordChanged' => array('type' => 'dt', 'min' => 0, 'guard' => true),
+    );
+
+    protected $loginname    = '';
+
+    /**
+     * Wrap the parent constructor and set roles if passed
+     *
+     * @param bool|int|array $a pkey value|set defaults|set values
+     * @param bool           $b Set defaults
+     *
+     * @throws \Exception
+     */
+    public function __construct($a = null, $b = null) {
+        if ('' == static::$query) {
+            self::$query = 'SELECT t.`log_id`, t.`tableName`, t.`login_id`, t.`eventDate`,'
+                .' t.`data`, t.`affected_id`, l.`loginname`'
+                .' FROM `'.self::$table.'` t LEFT JOIN `'.Login::getTable().'` l ON t.`login_id` = l.`login_id`';
+        }
+        if (G::$S->Login) {
+            self::$vars['login_id']['def'] = G::$S->Login->login_id;
+        }
+        if (is_array($a) && isset($a['loginname'])) {
+            $this->loginname = $a['loginname'];
+        }
+        parent::__construct($a, $b);
+    }
+
+    /**
+     * Logs data into the config database
+     *
+     * @param string $table the table name
+     * @param int    $key   the affected_id
+     * @param string $data  the data being saved
+     *
+     * @return string
+     */
+    public static function log($table, $key, $data) {
+        $C = new static(array('tableName' => $table,
+                              'affected_id' => $key,
+                              'data' => $data,
+                              ),
+                        true);
+        return G::build(DataBroker::class)->save($C);
+    }
+
+    /**
+     * Returns the last 100 log entries
+     *
+     * @param string $table the table name
+     * @param int    $key   the affected ID
+     *
+     * @return array
+     */
+    public static function getLog($table, $key) {
+        $C = new static(array('tableName' => $table, 'affected_id' => $key));
+        return $C->search(100, 0, 'log_id', true);
+    }
+
+    /**
+     * Sets the login name after load
+     *
+     * @param array $row the row
+     *
+     * @return array
+     */
+    public function onload(array $row = array()) {
+        if (isset($row['loginname'])) {
+            $this->loginname = $row['loginname'];
+            unset($row['loginname']);
+        }
+        return $row;
+    }
+
+    /**
+     * Returns the login name
+     *
+     * @return string
+     */
+    public function loginname() {
+        return $this->loginname;
+    }
+}

src/templates/header.php
@@ -1,0 +1,57 @@
+<?php use Stationer\Graphite\G; ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title><?php html($_title); ?></title>
+        <base href="<?php html($_siteURL); ?>">
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<?php foreach ($_meta as $k => $v) { ?>
+        <meta name="<?php html($k)?>" content="<?php html($v)?>">
+<?php }
+      foreach ($_script as $v) { ?>
+        <script type="text/javascript" src="<?php html($v)?>"></script>
+<?php }
+      foreach ($_link as $v) { ?>
+        <link rel="<?php html($v['rel'])?>" type="<?php html($v['type'])?>" href="<?php html($v['href'])?>" title="<?php html($v['title'])?>">
+<?php }
+      echo $_head;
+?>
+    </head>
+    <body>
+        <header id="header">
+            <h1 id="logo"><span><?php html($_siteName)?></span></h1>
+            <div id="login"><?php
+if ($_login_id) {
+        echo 'Hello, '.$_loginname
+            .'. (<a href="'.$_logoutURL.'">Logout</a> | '
+            .'<a href="/Account/edit" title="Your Account Settings">Account</a>)'
+            ;
+} else {
+        echo '(<a id="_loginLink" href="'.$_loginURL.'?_Lbl=Back&amp;_URI='.urlencode($_SERVER["REQUEST_URI"]).'">Login</a>)'
+            .'<script type="text/javascript">document.getElementById(\'_loginLink\').href += encodeURIComponent(location.hash);</script>';
+}
+            ?></div>
+            <nav>
+                <a href="/" title="Home Page">Home</a>
+                <a href="/Home/Contact" title="Contact">Contact</a>
+<?php if (G::$S && G::$S->Login && G::$S->roleTest('Admin')) { ?>
+                <a href="/Admin">Admin</a>
+<?php } ?>
+            </nav>
+            <div class="clear"></div>
+        </header>
+
+<?php G::$V->render('subheader');
+
+if (0 < $v = count($a = G::msg())) { ?>
+        <details id="msg" open="open">
+            <summary><?php echo $v;?> Messages:</summary>
+            <ul>
+<?php foreach ($a as $v) { ?>
+                <li class="<?php echo $v[1]; ?>"><?php echo $v[0]; ?></li>
+<?php } ?>
+            </ul>
+        </details>
+<?php } ?>
+
+        <section id="body">