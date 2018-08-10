a470730ce05990fa3a3dc1b4cbbf7553bb16171d
Tyler Uebele <tyler.uebele@gmail.com>
2018-08-10 11:20:02 -0400

    Add module config

---
+++ src/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Module config.
+ *
+ * Namespace to Stationer\Graphite
+ */
+
+namespace Stationer\Graphite;
+
+G::$G['namespaces'][] = '\\Stationer\\Barrel\\';
+G::$G['namespaces'][] = '\\Stationer\\Barrel\\data\\';
+G::$G['namespaces'][] = '\\Stationer\\Barrel\\models\\';
+G::$G['namespaces'][] = '\\Stationer\\Barrel\\controllers\\';
